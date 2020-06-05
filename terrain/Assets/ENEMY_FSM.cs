bc314a6fde9807d1073ce6732cfdc38376d559bd
Usama200 <65049632+Usama200@users.noreply.github.com>
2020-06-05 17:55:06 +0500

    My Assignment 3

---
+++ terrain/Assets/ENEMY_FSM.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENEMY_FSM : MonoBehaviour
+{
+    public enum enemy_state { PATROL, CHASE, ATTACK };
+    [SerializeField]
+    private enemy_state currentstate;
+    private Health playerhealth = null;
+    public float maxDamage = 10f;
+    public enemy_state CurrentState
+    {
+        get
+        {
+            return currentstate;
+        }
+        set
+        {
+            currentstate = value;
+            StopAllCoroutines();
+            switch (currentstate)
+            {
+                case enemy_state.PATROL:
+                    StartCoroutine(EnemyPatrol());
+                    break;
+                case enemy_state.CHASE:
+                    StartCoroutine(EnemyChase());
+                    break;
+                case enemy_state.ATTACK:
+                    StartCoroutine(EnemyAttack());
+                    break;
+            }
+        }
+    }
+
+    private checkmyvision checkmyVision;
+    private UnityEngine.AI.NavMeshAgent agent = null;
+    private Transform playertransform = null;
+    private Transform patrolDestination = null;
+
+    private void Awake()
+    {
+        checkmyVision = GetComponent<checkmyvision>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        playerhealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        playertransform = playerhealth.GetComponent<Transform>();
+
+    }
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        GameObject[] destination = GameObject.FindGameObjectsWithTag("Dest");
+        patrolDestination = destination[Random.Range(0, destination.Length)].GetComponent<Transform>();
+        currentstate = enemy_state.PATROL;
+
+
+    }
+    public IEnumerator EnemyPatrol()
+    {
+        while (currentstate == enemy_state.PATROL)
+        {
+            checkmyVision.sensitity = checkmyvision.enmsensitity.High;
+            agent.isStopped = false;
+            agent.SetDestination(patrolDestination.position);
+
+            while (agent.pathPending)
+                yield return null;
+
+            if (checkmyVision.targetInSight)
+            {
+                agent.isStopped = true;
+                currentstate = enemy_state.CHASE;
+                yield break;
+            }
+            yield break;
+        }
+
+    }
+    public IEnumerator EnemyChase()
+    {
+        while (currentstate == enemy_state.CHASE)
+        {
+            checkmyVision.sensitity = checkmyvision.enmsensitity.Low;
+            agent.isStopped = false;
+            agent.SetDestination(checkmyVision.lastKhownSighting);
+
+            while (agent.pathPending)
+            {
+                yield return null;
+            }
+
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                agent.isStopped = true;
+                if (!checkmyVision.targetInSight)
+                    currentstate = enemy_state.PATROL;
+                else
+                    currentstate = enemy_state.ATTACK;
+                yield break;
+            }
+            yield return null;
+        }
+
+    }
+    public IEnumerator EnemyAttack()
+    {
+        while (currentstate == enemy_state.ATTACK)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(playertransform.position);
+            while (agent.pathPending)
+                yield return null;
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                currentstate = enemy_state.CHASE;
+            }
+            else
+            {
+                playerhealth.healthpoints -= maxDamage * Time.deltaTime;
+            }
+            yield return null;
+        }
+        yield break;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ terrain/Assets/checkmyvision.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkmyvision : MonoBehaviour
+{
+
+   
+    
+    public enum enmsensitity { High, Low };
+    public enmsensitity sensitity = enmsensitity.High;
+
+    public bool targetInSight = false;
+
+    public float fieldofvision = 45f;
+
+    public Transform target = null;
+
+    public Transform Myeyes= null;
+
+    public Transform npcTranform = null;
+
+    private SphereCollider sphereCollider = null;
+
+    public Vector3 lastKhownSighting = Vector3.zero;
+
+
+    // Start is called before the first frame update
+
+
+    public void awake()
+    {
+        npcTranform = GetComponent<Transform>();
+        sphereCollider = GetComponent<SphereCollider>();
+        lastKhownSighting = npcTranform.position;
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
+    }
+
+    bool InMyFieldOfVision()
+    {
+        Vector3 dirTotarget = target.position - Myeyes.position;
+
+        float angle = Vector3.Angle(Myeyes.forward, dirTotarget);
+
+        if (angle <= fieldofvision)
+            return true;
+        else
+            return false;
+    }
+
+
+    bool ClearLineOfSight()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(Myeyes.position,(target.position - Myeyes.position).normalized,out hit , sphereCollider.radius))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                return true;
+            }
+                
+        }
+        return false;
+    }
+
+    void updateSight()
+    {
+        switch (sensitity)
+        {
+            case enmsensitity.High:
+                targetInSight = InMyFieldOfVision() && ClearLineOfSight();
+                break;
+            case enmsensitity.Low:
+                targetInSight = InMyFieldOfVision() || ClearLineOfSight();
+                break;
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        updateSight();
+        if (targetInSight)
+            lastKhownSighting = target.position;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+        targetInSight = false;
+
+    }
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
